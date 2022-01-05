d7cdfa9129936b520f20dc3d97e6a839f9911b1f
Sive -K <74187896+iloveass818@users.noreply.github.com>
2022-01-05 16:29:20 +0000

    fixed ai chat api working in unintended channels

--- cogs/ChatBot.py
+++ cogs/ChatBot.py
@@ -16,16 +16,19 @@ class ChatBot(commands.Cog):
     async def on_message(self, msg):
         if  msg.author == self.client.user:
             return
-        api_key = os.environ['Ai_key']
-        headers = {"authorization":api_key,
-        "x-rapidapi-host":"random-stuff-api.p.rapidapi.com",
-        "x-rapidapi-key": "633b3310bbmshb06853853f96fcbp1986d1jsnae275e323f14"}
+        if msg.channel.id == 843820294803947561:
+            api_key = os.environ['Ai_key']
+            headers = {"authorization":api_key,"x-rapidapi-host":"random-stuff-api.p.rapidapi.com",
+            "x-rapidapi-key": "633b3310bbmshb06853853f96fcbp1986d1jsnae275e323f14"}
+            url = "https://random-stuff-api.p.rapidapi.com/ai"
+            params = {"msg":msg.content,"bot_name":"DatteBot","bot_gender":"male","bot_master":"iloveass2#8380","bot_age":"8","bot_company":"iloveass2 sons Pvt. Ltd.","bot_location":"India","bot_email":"googlesoftpplezonkart@.com","bot_build":"Public","bot_birth_year":"2021","bot_birth_date":"23rd April, 2021","bot_birth_place":"India","bot_favorite_color":"Blue","bot_favorite_book":"Harry Potter","bot_favorite_band":"Imagine Doggos","bot_favorite_artist":"Eminem","bot_favorite_actress":"Zendaya","bot_favorite_actor":"Ryan reynolds"}
+            resp = requests.request("GET", url, headers=headers, params=params)
+            text = resp.json()
+            await msg.reply(text["AIResponse"])
 
-        url = "https://random-stuff-api.p.rapidapi.com/ai"
-        params = {"msg":msg.content,
-        "bot_name":"DatteBot","bot_gender":"male","bot_master":"iloveass2#8380","bot_age":"8","bot_company":"iloveass2 sons Pvt. Ltd.","bot_location":"India","bot_email":"googlesoftpplezonkart@.com","bot_build":"Public","bot_birth_year":"2021","bot_birth_date":"23rd April, 2021","bot_birth_place":"India","bot_favorite_color":"Blue","bot_favorite_book":"Harry Potter","bot_favorite_band":"Imagine Doggos","bot_favorite_artist":"Eminem","bot_favorite_actress":"Zendaya","bot_favorite_actor":"Ryan reynolds"}
 
-        resp = requests.request("GET", url, headers=headers, params=params)
-        text = resp.json()
-        await msg.reply(text["AIResponse"])
+        else:
+            pass
+
+        
 
